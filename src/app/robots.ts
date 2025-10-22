d41c9573e70419e59b11506ce9188a0488bd54a9
hiteshshetty-dev <hitesh.shetty@contentstack.com>
2025-10-22 15:03:09 +0530

    feat: update robots.txt to disallow access to /dev/ directory

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: [],
+      disallow: ["/dev/"],
     },
     sitemap: "https://hiteshshetty.com/sitemap.xml",
   };
