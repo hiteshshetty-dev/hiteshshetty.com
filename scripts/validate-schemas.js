7f6d4befd7c5ddfcc15a7324f497105971b6d343
hiteshshetty-dev <hitesh.shetty@contentstack.com>
2025-11-08 15:08:36 +0530

    chore: fix lint issue with schema

--- scripts/validate-schemas.js
+++ scripts/validate-schemas.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require("node:fs");
+const path = require("node:path");
 const Ajv = require("ajv");
 const addFormats = require("ajv-formats");
 
